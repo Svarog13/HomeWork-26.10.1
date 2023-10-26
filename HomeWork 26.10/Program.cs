7e96e43b5c225b6d4b5c73b48df41d3c382493aa
Void <Void@SWEETVOID1488>
2023-10-26 18:40:42 +0300

    Add project files.

---
+++ HomeWork 26.10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the side length of the square: ");
+        int sideLength = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the character to draw the square: ");
+        char character = char.Parse(Console.ReadLine());
+
+        DrawSquare(sideLength, character);
+    }
+
+    static void DrawSquare(int sideLength, char character)
+    {
+        if (sideLength <= 0)
+        {
+            Console.WriteLine("Side length must be a positive integer.");
+            return;
+        }
+        for (int i = 0; i < sideLength; i++)
+        {
+            for (int j = 0; j < sideLength; j++)
+            {
+                Console.Write(character + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
